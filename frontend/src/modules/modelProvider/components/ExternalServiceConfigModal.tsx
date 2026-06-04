7ba66ba722bdc8f0fb8203855c6b8a1f352059c3
WangDaBenShi <15830881993@163.com>
2026-06-04 23:31:53 +0800

    fix external service custom base url config

--- frontend/src/modules/modelProvider/components/ExternalServiceConfigModal.tsx
+++ frontend/src/modules/modelProvider/components/ExternalServiceConfigModal.tsx
@@ -314,7 +314,7 @@ export default function ExternalServiceConfigModal({
       destroyOnClose
       footer={[
         <Button key="close" onClick={handleClose}>
-          {t("common.close")}
+          {t("modelProvider.external.saveConfig")}
         </Button>,
       ]}
       onCancel={handleClose}
